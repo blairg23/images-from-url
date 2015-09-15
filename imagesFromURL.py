f7e3a9ecf9ba5e6f213dbb97fde46a6feadf2aeb
Blair Gemmer <blair.gemmer@gmail.com>
2015-09-15 03:29:46 -0600

    Successfully dumped image from external url list

--- imagesFromURL.py
+++ imagesFromURL.py
@@ -1,8 +1,20 @@
 import requests
+import os
+
 from bs4 import BeautifulSoup
 
+def dump_image_files(url_list=None, folder_name=None):
+    for url in url_list:
+        response = requests.get(url, stream=True)        
+        if response.status_code == 200:            
+            image_name = url.split('/')[-1].rstrip()
+            full_path = os.path.join('images', folder_name, image_name)
+            if not os.path.exists(os.path.dirname(full_path)): # If the folder doesn't exist yet,
+                os.makedirs(os.path.dirname(full_path)) # Create it
+            with open(full_path, 'wb') as out_file: # Then write to it               
+                out_file.write(response.content)
 
-def retrieveImages(url=None, debug=False):
+def retrieve_image_urls(url=None, debug=False):
     images = []
     html_file = requests.get(url)
     soup = BeautifulSoup(html_file.content, 'html.parser')
@@ -17,18 +29,20 @@ def retrieveImages(url=None, debug=False):
     image_urls = []
     for image in images:
         if image[0] == '/': # If the image url is a relative link
-            image_url = url+image # Add the original url as a prefix
+            image_url = 'http:' + image # Add the original url as a prefix
         else:
             image_url = image
-        image_urls.append(image_url)
+        image_urls.append(str(image_url))
     return image_urls
 
 url_list = 'urls.txt' # File with list of urls
 with open(url_list, 'r') as infile:
     for url in infile.readlines():
-        originalUrl = url
+        original_url = url.rstrip() # Strip that nasty \n
         # numberOfPages = 1
-        title = originalUrl.split('/')[-1].rstrip()
-        print originalUrl
+        title = original_url.split('/')[-1].rstrip() # Strip that nasty \n
+        print original_url
         print title
-        retrieveImages(originalUrl)
+        image_urls = retrieve_image_urls(url=original_url)
+        print image_urls
+        dump_image_files(url_list=image_urls, folder_name=title)
