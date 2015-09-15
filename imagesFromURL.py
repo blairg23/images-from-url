592163ce4e4e3c0f652911dca76d104f5a2240bc
Blair Gemmer <blair.gemmer@gmail.com>
2015-09-15 03:49:21 -0600

    Started feature: Get images from source file

--- imagesFromURL.py
+++ imagesFromURL.py
@@ -1,9 +1,15 @@
 import requests
 import os
+import json
 
 from bs4 import BeautifulSoup
 
 def dump_image_files(url_list=None, folder_name=None, gallery_title=None):
+    '''
+    Given a list of urls (which can have 1 or more url elements), will dump all the images
+    into a folder called /images/<gallery_title>/<folder_name>/, where <gallery_title> and
+    <folder_name> are specified as parameters. 
+    '''
     for url in url_list:
         response = requests.get(url, stream=True)
         if response.status_code == 200:
@@ -17,7 +23,36 @@ def dump_image_files(url_list=None, folder_name=None, gallery_title=None):
             else:
                 print 'File already exists, ignoring.'
 
-def retrieve_image_urls(url=None, debug=False):
+def retrieve_image_urls_from_source(source_file=None, class_name=None, debug=False):
+    '''
+    Given an html file or text document, will dump all the images into a folder called
+    /images/<folder_name>/, where <folder_name> is specified as a parameter.
+    ''' 
+    images = []
+    html_file = ''
+    with open(source_file, 'r') as infile:
+        for line in infile.readlines():
+            html_file += line
+    soup = BeautifulSoup(html_file, 'html.parser')
+
+    if debug:
+        print soup.prettify()
+    pass
+    
+    for link in soup.find_all('a'):
+        if '.jpg' in link.get('href') or '.png' in link.get('href'):
+            images.append(link.get('href'))
+
+    image_urls = []
+    for image in images:
+        if image[0] == '/': # If the image url is a relative link
+            image_url = url+image # Add the original url as a prefix
+        else:
+            image_url = image
+        image_urls.append(str(image_url))
+    return image_urls
+
+def retrieve_image_urls_from_url(url=None, debug=False):
     images = []
     html_file = requests.get(url)
     soup = BeautifulSoup(html_file.content, 'html.parser')
@@ -38,14 +73,18 @@ def retrieve_image_urls(url=None, debug=False):
         image_urls.append(str(image_url))
     return image_urls
 
+
 def get_gallery_images(gallery_urls=None, gallery_title=None):
+    '''
+    Given a list of gallery urls, will gather image urls and dump the image files.
+    '''
     url_list = gallery_urls # File with list of urls
     for url in url_list:
         original_url = url.rstrip() # Strip that nasty \n
         title = original_url.split('/')[-1].rstrip() # Strip that nasty \n
         print original_url
         print title
-        image_urls = retrieve_image_urls(url=original_url)
+        image_urls = retrieve_image_urls_from_url(url=original_url)
         dump_image_files(url_list=image_urls, folder_name=title, gallery_title=gallery_title)
 
 
@@ -73,13 +112,32 @@ def retrieve_gallery_urls(url=None, class_name=None, debug=False):
 
     return gallery_urls
 
-def get_images_from_list(url_list='urls.txt'):
-    with open(url_list, 'r') as infile:
-        for url in infile.readlines():
-            page = 1 # For multiple pages
-            original_url = url.rstrip() # Strip that nasty \n       
-            title = original_url.split('/')[-1].rstrip() # Strip that nasty \n      
-            while requests.get(original_url).status_code == 200: # As long as we're working with a real url
-                gallery_urls = retrieve_gallery_urls(url=original_url, class_name='track') # This class name signifies a gallery (in this case, 'track')
-                get_gallery_images(gallery_urls=gallery_urls, gallery_title=title)
-                original_url += '?page='+str(page)
+def get_galleries_from_list(url_list='data/gallery_urls.json'):
+    '''
+    Retrieve images from a list of gallery urls.
+    '''
+    pass
+    # with open(url_list, 'r') as infile:
+    #   for url in infile.readlines():
+    #       page = 1 # For multiple pages
+    #       original_url = url.rstrip() # Strip that nasty \n       
+    #       title = original_url.split('/')[-1].rstrip() # Strip that nasty \n      
+    #       while requests.get(original_url).status_code == 200: # As long as we're working with a real url
+    #           gallery_urls = retrieve_gallery_urls(url=original_url, class_name='track') # This class name signifies a gallery (in this case, 'track')
+    #           get_gallery_images(gallery_urls=gallery_urls, gallery_title=title)
+    #           original_url += '?page='+str(page)
+
+
+
+
+
+source_file = 'data/source.html'
+title = 'suicide-girls'
+retrieve_image_urls_from_source(source_file=source_file, folder_name=title)
+
+
+#get_images_from_list()
+
+
+# single_url = ''
+# retrieve_image_urls_from_url
