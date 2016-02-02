a31d99bc36c5a1b42b72be89d662a237d1d34551
blairg23 <blairg23@gmail.com>
2016-02-02 05:40:00 -0700

    Updated instagram account

--- instagram_client.py
+++ instagram_client.py
@@ -82,7 +82,7 @@ class InstagramClient():
 if __name__ == '__main__':
 	client = InstagramClient()
 	if client.authenticate():
-		screen_name = 'lesss_iss_more'
+		screen_name = 'blair.gemmer'
 
 		user_id = client.get_user_id(screen_name=screen_name)
 
