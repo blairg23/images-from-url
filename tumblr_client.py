e195726e382ed03a29d55496365362b64f4284d1
blair gemmer <blairg23@gmail.com>
2017-05-21 03:15:53 -0600

    Updated requirements files and tumblr script to support Python3

--- tumblr_client.py
+++ tumblr_client.py
@@ -17,18 +17,18 @@ debug=False
 # Make the first request
 photo_urls = []
 blog_name = 'blairgemmer'
-req = client.posts(blog_name, type="photo")
+req = client.posts(blog_name, type='photo')
 number_of_posts = req['total_posts']
 
 if debug:
-	print number_of_posts
+	print(number_of_posts)
 	for item in req:
-		print item
+		print(item)
 
 # Process all the blog posts as pages of posts
 offset_number = 20
 number_of_pages = int(math.ceil(number_of_posts/offset_number*1.))
-print "Grabbing " + str(number_of_posts) + " photos from " + str(blog_name) + ".tumblr.com..."
+print('Grabbing ' + str(number_of_posts) + ' photos from ' + str(blog_name) + '.tumblr.com...')
 
 directory = os.path.join('images', 'tumblr', blog_name)
 # If folder doesn't exist, create it:
@@ -41,8 +41,8 @@ contents = os.path.join(directory,'contents.txt')
 for i in range(number_of_pages+3):	
 #for i in range(1):
 	if debug:
-		print "Page=",i
-	req = client.posts(blog_name, type="photo", offset=i*offset_number) # to increment the offset by 20 every iteration	
+		print('Page=',i)
+	req = client.posts(blog_name, type='photo', offset=i*offset_number) # to increment the offset by 20 every iteration	
 	count=0
 	#print json.dumps(req, indent=4)
 	for post in req['posts']:			
@@ -50,7 +50,7 @@ for i in range(number_of_pages+3):
 		for photo in post['photos']:
 			#print json.dumps(photo['original_size']['url'], indent=4)	
 			url = photo['original_size']['url']								
-			print 'Downloading {url}'.format(url=url)
+			print('Downloading {url}'.format(url=url))
 			if os.path.exists(contents):
 				with open(contents, 'r') as infile:
 					lines = infile.readlines()
@@ -61,9 +61,9 @@ for i in range(number_of_pages+3):
 					outfile.write(url+'\n')
 				wget.download(url, out=directory) # Download each file
 			else:
-				print filename, " exists."
+				print(filename, ' exists.')
 	if debug:
-		print "Added " + str(count) + " photos.\n"
+		print('Added ' + str(count) + ' photos.\n')
 	
 	
-print "Finished downloading photos."
+print('Finished downloading photos.')
